f5683b9ed7856bfadc923c5ed04a2373138e8c73
Karel Horsky <karel.horsky@centrum.cz>
2017-11-25 11:08:21 +0100

    add first test

---
+++ tests/HelloWorldTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace PosledniSobota\Tests;
+
+use PosledniSobota\HelloWorld;
+
+class HelloWorldTest extends \PHPUnit\Framework\TestCase
+{
+	public function testHelloWorld()
+	{
+		$helloWorld = new HelloWorld();
+	}
+}
